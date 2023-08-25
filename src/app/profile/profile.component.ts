d29309ba846d92d602614809809156820c7afe1a
haru0414 <skb900414@gmail.com>
2023-08-25 14:40:14 +0800

    no message

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  mugShot = '../assets/img/mugShot.jpg';
+  mugShot = '../resume-page/assets/img/mugShot.jpg';
   constructor() {}
 
   ngOnInit(): void {}
